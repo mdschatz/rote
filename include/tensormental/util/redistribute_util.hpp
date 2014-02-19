a123fa18ac060d31f6de0862bb7ddc8a39b729a7
Martin Schatz <martin.schatz@utexas.edu>
2014-02-19 01:41:19 -0600

    Forgot files

---
+++ include/tensormental/util/redistribute_util.hpp
@@ -0,0 +1,28 @@
+/*
+   Copyright (c) 2009-2013, Jack Poulson
+   All rights reserved.
+
+   This file is part of Elemental and is under the BSD 2-Clause License,
+   which can be found in the LICENSE file in the root directory, or at
+   http://opensource.org/licenses/BSD-2-Clause
+*/
+
+#pragma once
+#ifndef TMEN_CORE_UTIL_REDISTRIBUTE_UTIL_DECL_HPP
+#define TMEN_CORE_UTIL_REDISTRIBUTE_UTIL_DECL_HPP
+
+#include "tensormental/core/imports/mpi.hpp"
+#include "tensormental/core/imports/choice.hpp"
+#include "tensormental/core/imports/mpi_choice.hpp"
+
+#include <vector>
+#include <iostream>
+#include "tensormental/core.hpp"
+
+namespace tmen{
+
+template <typename T>
+void DetermineRSCommunicateDataSize(const DistTensor<T>& B, const int reduceIndex, int& recvSize, int& sendSize);
+
+}
+#endif // ifndef TMEN_CORE_UTIL_REDISTRIBUTE_UTIL_DECL_HPP
