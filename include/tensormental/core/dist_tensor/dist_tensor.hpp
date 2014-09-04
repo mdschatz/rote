78ddeaece9e696fcd93fa1851ab57233fc884959
Martin Schatz <martin.schatz@utexas.edu>
2014-09-04 12:43:43 -0500

    Added Multi-mode ReduceScatterUpdateRedistFrom redistribution

--- include/tensormental/core/dist_tensor/dist_tensor.hpp
+++ include/tensormental/core/dist_tensor/dist_tensor.hpp
@@ -310,6 +310,7 @@ public:
     //
     void PartialReduceScatterRedistFrom(const DistTensor<T>& A, const Mode reduceScatterMode);
     void ReduceScatterRedistFrom(const DistTensor<T>& A, const ModeArray& reduceModes, const ModeArray& scatterModes);
+    void ReduceScatterUpdateRedistFrom(const DistTensor<T>& A, const T beta, const ModeArray& reduceModes, const ModeArray& scatterModes);
     void ReduceScatterRedistFrom(const DistTensor<T>& A, const Mode reduceMode, const Mode scatterMode);
     void ReduceScatterUpdateRedistFrom(const DistTensor<T>& A, const T beta, const Mode reduceMode, const Mode scatterMode);
 
