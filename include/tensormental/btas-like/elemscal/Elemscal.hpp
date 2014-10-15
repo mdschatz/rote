e9820c8fcc3ddcb2a1abb5683df5c52bd5e34485
Martin Schatz <martin.schatz@utexas.edu>
2014-10-15 13:31:14 -0500

    Fast version ElemScal implemented

--- include/tensormental/btas-like/elemscal/Elemscal.hpp
+++ include/tensormental/btas-like/elemscal/Elemscal.hpp
@@ -50,6 +50,65 @@ ElemScalHelper(const Tensor<T>& A, const Tensor<T>& B, Mode mode, T const * cons
     }
 }
 
+template<typename T>
+inline void
+ElemScal_fast(const Tensor<T>& A, const Tensor<T>& B, Mode mode, T const * const src1Buf, T const * const src2Buf,  T * const dstBuf, const ElemScalData& data ){
+    Unsigned packSlice;
+//    printf("ping packcommHelper\n");
+
+    const std::vector<Unsigned> loopEnd = data.loopShape;
+    const std::vector<Unsigned> src1BufStrides = data.src1Strides;
+    const std::vector<Unsigned> src2BufStrides = data.src2Strides;
+    const std::vector<Unsigned> dstBufStrides = data.dstStrides;
+    Unsigned src1BufPtr = 0;
+    Unsigned src2BufPtr = 0;
+    Unsigned dstBufPtr = 0;
+    Unsigned order = loopEnd.size();
+    Location curLoc(order, 0);
+    Unsigned ptr = 0;
+    Unsigned i;
+//    std::string ident = "";
+//    for(i = 0; i < packData.loopShape.size() - packMode; i++)
+//        ident += "  ";
+
+    if(loopEnd.size() == 0){
+        dstBuf[0] = src1Buf[0] * src2Buf[0];
+        return;
+    }
+
+    bool done = !ElemwiseLessThan(curLoc, loopEnd);
+
+    while(!done){
+
+        dstBuf[dstBufPtr] = src1Buf[src1BufPtr] * src2Buf[src2BufPtr];
+        //Update
+        curLoc[ptr]++;
+        dstBufPtr += dstBufStrides[ptr];
+        src1BufPtr += src1BufStrides[ptr];
+        src2BufPtr += src2BufStrides[ptr];
+        while(ptr < order && curLoc[ptr] >= loopEnd[ptr]){
+            curLoc[ptr] = 0;
+
+            dstBufPtr -= dstBufStrides[ptr] * (loopEnd[ptr]);
+            src1BufPtr -= src1BufStrides[ptr] * (loopEnd[ptr]);
+            src2BufPtr -= src2BufStrides[ptr] * (loopEnd[ptr]);
+            ptr++;
+            if(ptr >= order){
+                done = true;
+                break;
+            }else{
+                curLoc[ptr]++;
+                dstBufPtr += dstBufStrides[ptr];
+                src1BufPtr += src1BufStrides[ptr];
+                src2BufPtr += src2BufStrides[ptr];
+            }
+        }
+        if(done)
+            break;
+        ptr = 0;
+    }
+}
+
 //NOTE: Add checks for conforming shapes
 //NOTE: Convert to incorporate blocked tensors.
 template <typename T>
@@ -68,7 +127,11 @@ void ElemScal(const Tensor<T>& A, const Tensor<T>& B, Tensor<T>& C){
     const T* src2Buf = B.LockedBuffer();
     T* dstBuf = C.Buffer();
 
+#ifndef RELEASE
     ElemScalHelper(A, B, order-1, src1Buf, src2Buf, dstBuf, data);
+#else
+    ElemScal_fast(A, B, order - 1, src1Buf, src2Buf, dstBuf, data);
+#endif
 }
 
 ////////////////////////////////////
