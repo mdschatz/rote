257d0bb230c3ae3831db250b419119a54d6351b1
Martin Schatz <martin.schatz@utexas.edu>
2015-01-24 17:49:08 -0600

    Implemented fast version of LocalReduce

--- include/tensormental/btas-like/level1/Reduce.hpp
+++ include/tensormental/btas-like/level1/Reduce.hpp
@@ -88,6 +88,57 @@ void LocalReduceElemSelectHelper(const Unsigned elemMode, const ModeArray& nonRe
     }
 }
 
+template <typename T>
+void LocalReduceElemSelect_fast(const Unsigned elemMode, const ModeArray& nonReduceModes, const ModeArray& reduceModes, const ObjShape& reduceShape, T const * const srcBuf, const std::vector<Unsigned>& srcStrides, T * const dstBuf, const std::vector<Unsigned>& dstStrides){
+    const std::vector<Unsigned> loopEnd = reduceShape;
+
+    Unsigned srcBufPtr = 0;
+    Unsigned dstBufPtr = 0;
+    Unsigned order = loopEnd.size();
+    Unsigned nonReduceOrder = nonReduceModes.size();
+    Location curLoc(order, 0);
+    Unsigned modePtr = 0;
+    Unsigned ptr = nonReduceModes[0];
+
+    ObjShape shapeToReduce = FilterVector(reduceShape, reduceModes);
+    std::vector<Unsigned> reduceStrides = FilterVector(srcStrides, reduceModes);
+
+    bool done = !ElemwiseLessThan(curLoc, loopEnd);
+
+    if(!done && order == 0){
+        LocalReduce_fast(reduceModes, shapeToReduce, &(srcBuf[0]), reduceStrides, &(dstBuf[0]));
+        return;
+    }
+
+    while(!done){
+        LocalReduce_fast(reduceModes, shapeToReduce, &(srcBuf[srcBufPtr]), reduceStrides, &(dstBuf[dstBufPtr]));
+        //Update
+        curLoc[ptr]++;
+        srcBufPtr += srcStrides[ptr];
+        dstBufPtr += dstStrides[ptr];
+        while(modePtr < nonReduceOrder && curLoc[ptr] >= loopEnd[ptr]){
+            curLoc[ptr] = 0;
+
+            srcBufPtr -= srcStrides[ptr] * (loopEnd[ptr]);
+            dstBufPtr -= dstStrides[ptr] * (loopEnd[ptr]);
+            modePtr++;
+            if(modePtr >= nonReduceOrder){
+                done = true;
+                break;
+            }else{
+                ptr = nonReduceModes[modePtr];
+                curLoc[ptr]++;
+                srcBufPtr += srcStrides[ptr];
+                dstBufPtr += dstStrides[ptr];
+            }
+        }
+        if(done)
+            break;
+        modePtr = 0;
+        ptr = nonReduceModes[0];
+    }
+}
+
 ////////////////////////////////////
 // Local interfaces
 ////////////////////////////////////
@@ -114,7 +165,8 @@ void LocalReduce(const Tensor<T>& A, Tensor<T>& B, const Permutation& permBToA, 
     const std::vector<Unsigned> srcStrides = A.Strides();
     const std::vector<Unsigned> dstStrides = PermuteVector(B.Strides(), permBToA);
 
-    LocalReduceElemSelectHelper(nonReduceModes.size() - 1, nonReduceModes, reduceModes, A.Shape(), A.LockedBuffer(), srcStrides, B.Buffer(), dstStrides);
+//    LocalReduceElemSelectHelper(nonReduceModes.size() - 1, nonReduceModes, reduceModes, A.Shape(), A.LockedBuffer(), srcStrides, B.Buffer(), dstStrides);
+    LocalReduceElemSelect_fast(nonReduceModes.size() - 1, nonReduceModes, reduceModes, A.Shape(), A.LockedBuffer(), srcStrides, B.Buffer(), dstStrides);
 }
 
 template <typename T>
