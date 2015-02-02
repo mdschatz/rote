f0b04cf4e7a70c1e00897faccb1d06425d94a5a1
Martin Schatz <martin.schatz@utexas.edu>
2015-02-02 14:59:38 -0600

    Forgot to commit A2AP2P changes.

--- src/core/dist_tensor/redistribute/a2aInterfaces.cpp
+++ src/core/dist_tensor/redistribute/a2aInterfaces.cpp
@@ -26,46 +26,46 @@ void DistTensor<T>::AllToAllRedistFrom(const DistTensor<T>& A, const ModeArray& 
     std::sort(sortedCommModes.begin(), sortedCommModes.end());
 
     //BEGIN A2A code
-//    A2AP2PData optData = DetermineA2AP2POptData(A, commModes);
+    A2AP2PData optData = DetermineA2AP2POptData(A, commModes);
 
-//    int commRank = mpi::CommRank(MPI_COMM_WORLD);
-//    if(commRank == 0){
-//        PrintA2AOptData(optData, "optData");
-//    }
+    int commRank = mpi::CommRank(MPI_COMM_WORLD);
+    if(commRank == 0){
+        PrintA2AOptData(optData, "optData");
+    }
 
-//    if(optData.doOpt == true){
-//        const tmen::Grid& g = A.Grid();
-//        ModeArray p2pModes = optData.p2pModes;
-//        ModeArray a2aModes = optData.a2aModes;
-//        std::sort(a2aModes.begin(), a2aModes.end());
-//        ModeArray a2ap2pModes = ConcatenateVectors(a2aModes, p2pModes);
-//
-//        DistTensor<T> temp1(optData.opt1Dist, g);
-//        temp1.PermutationRedistFrom(A, a2ap2pModes);
-//        Print(temp1, "temp1");
-//        DistTensor<T> temp2(optData.opt2Dist, g);
-//        temp2.PermutationRedistFrom(temp1, p2pModes);
-//        Print(temp2, "temp2");
-//        temp1.EmptyData();
-//        DistTensor<T> temp3(optData.opt3Dist, g);
-//        temp3.PermutationRedistFrom(temp2, a2ap2pModes);
-//        Print(temp3, "temp3");
-//        temp2.EmptyData();
-//        DistTensor<T> temp4(optData.opt4Dist, g);
-//        temp4.ResizeTo(temp3);
-//        //NOTE: Fix this bug
-//        if(a2aModes.size() == 0)
-//            temp4.PermutationRedistFrom(temp3, a2aModes);
-//        else
-//            temp4.AllToAllCommRedist(temp3, a2aModes);
-//        Print(temp4, "temp4");
-//        temp3.EmptyData();
-//        PermutationRedistFrom(temp4, a2ap2pModes);
-//        Print(*this, "final");
-//        temp4.EmptyData();
-//    }else{
+    if(optData.doOpt == true){
+        const tmen::Grid& g = A.Grid();
+        ModeArray p2pModes = optData.p2pModes;
+        ModeArray a2aModes = optData.a2aModes;
+        std::sort(a2aModes.begin(), a2aModes.end());
+        ModeArray a2ap2pModes = ConcatenateVectors(a2aModes, p2pModes);
+
+        DistTensor<T> temp1(optData.opt1Dist, g);
+        temp1.PermutationRedistFrom(A, a2ap2pModes);
+        Print(temp1, "temp1");
+        DistTensor<T> temp2(optData.opt2Dist, g);
+        temp2.PermutationRedistFrom(temp1, p2pModes);
+        Print(temp2, "temp2");
+        temp1.EmptyData();
+        DistTensor<T> temp3(optData.opt3Dist, g);
+        temp3.PermutationRedistFrom(temp2, a2ap2pModes);
+        Print(temp3, "temp3");
+        temp2.EmptyData();
+        DistTensor<T> temp4(optData.opt4Dist, g);
+        temp4.ResizeTo(temp3);
+        //NOTE: Fix this bug
+        if(a2aModes.size() == 0)
+            temp4.PermutationRedistFrom(temp3, a2aModes);
+        else
+            temp4.AllToAllCommRedist(temp3, a2aModes);
+        Print(temp4, "temp4");
+        temp3.EmptyData();
+        PermutationRedistFrom(temp4, a2ap2pModes);
+        Print(*this, "final");
+        temp4.EmptyData();
+    }else{
         AllToAllCommRedist(A, sortedCommModes);
-//    }
+    }
     PROFILE_STOP;
 }
 
