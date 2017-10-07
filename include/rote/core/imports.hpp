f8ee8f480cf6bdc3321b4ed5db626fcfa7923dd4
Martin Schatz <martin.schatz@utexas.edu>
2017-10-06 22:21:14 -0700

    Add missing file

---
+++ include/rote/core/imports.hpp
@@ -0,0 +1,18 @@
+/*
+   Copyright (c) 2009-2013, Jack Poulson
+   All rights reserved.
+
+   This file is part of Elemental and is under the BSD 2-Clause License,
+   which can be found in the LICENSE file in the root directory, or at
+   http://opensource.org/licenses/BSD-2-Clause
+*/
+#pragma once
+#ifndef ROTE_CORE_IMPORTS_HPP
+#define ROTE_CORE_IMPORTS_HPP
+
+#include "rote/core/imports/blas.hpp"
+#include "rote/core/imports/choice.hpp"
+#include "rote/core/imports/mpi_choice.hpp"
+#include "rote/core/imports/mpi.hpp"
+
+#endif // ifndef ROTE_CORE_IMPORTS_HPP
